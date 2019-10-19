163639807a96900371fc3a76852287379196df45
Dmitry Mersiyanov <dmersiyanov@yandex.ru>
2019-10-19 08:50:36 +0300

    added PaperLocalStorage

--- app/src/main/java/com/calorificapp/CalorificApp.kt
+++ app/src/main/java/com/calorificapp/CalorificApp.kt
@@ -2,12 +2,20 @@ package com.calorificapp
 
 import android.app.Application
 import io.paperdb.Paper
+import timber.log.Timber
 
 class CalorificApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
         Paper.init(this)
+
+    }
+
+    private fun initLogger() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 
 }
---
+++ app/src/main/java/com/calorificapp/data/PaperLocalStorage.kt
@@ -0,0 +1,56 @@
+package com.calorificapp.data
+
+import com.calorificapp.domain.repo.LocalStorage
+import io.paperdb.Paper
+import io.reactivex.Completable
+import io.reactivex.Single
+import timber.log.Timber
+import javax.inject.Inject
+
+class PaperLocalStorage @Inject constructor() : LocalStorage {
+
+    override fun write(tableName: String, key: String, data: Any): Completable {
+        return Completable.fromAction {
+            try {
+                Paper.book(tableName).write(key, data)
+            } catch (e: Exception) {
+                Timber.d(e)
+                delete(tableName, key)
+            }
+        }
+    }
+
+    override fun <T : Any> read(tableName: String, key: String): Single<T> {
+        return Single.fromCallable {
+            try {
+                Paper.book(tableName).read<T>(key)?.let {
+                    it
+                } ?: throw Exception("No data in storage with key $key for table $tableName")
+            } catch (e: Exception) {
+                Timber.d(e)
+                delete(tableName, key)
+                throw Exception("Error read data with key $key for table $tableName")
+            }
+        }
+    }
+
+    override fun delete(tableName: String, key: String): Completable {
+        return Completable.fromAction {
+            try {
+                Paper.book(tableName).delete(key)
+            } catch (e: Exception) {
+                Timber.d(e)
+            }
+        }
+    }
+
+    override fun erase(tableName: String): Completable {
+        return Completable.fromAction {
+            try {
+                Paper.book(tableName).destroy()
+            } catch (e: Exception) {
+                Timber.d(e)
+            }
+        }
+    }
+}
