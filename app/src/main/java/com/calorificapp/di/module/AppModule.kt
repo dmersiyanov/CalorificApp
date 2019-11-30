b90d8a551362dc5d7609881617c44d70046db188
Dmitry Mersiyanov <dmersiyanov@yandex.ru>
2019-11-30 11:03:28 +0300

    deleted commented code

--- app/src/main/java/com/calorificapp/di/module/AppModule.kt
+++ app/src/main/java/com/calorificapp/di/module/AppModule.kt
@@ -7,28 +7,10 @@ import dagger.Provides
 
 
 @Module(includes = [ViewModelModule::class])
-public class AppModule {
-
+class AppModule {
 
     @Provides
     fun provideContext(application: Application): Context {
         return application
     }
-
-//    private val BASE_URL = "https://api.github.com/"
-
-//    @Singleton
-//    @Provides
-//    internal fun provideApp(): Retrofit {
-//        return Retrofit.Builder().baseUrl(BASE_URL)
-//            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build()
-//    }
-//
-//    @Singleton
-//    @Provides
-//    internal fun provideRetrofitService(retrofit: Retrofit): RepoService {
-//        return retrofit.create(RepoService::class.java)
-//    }
 }
